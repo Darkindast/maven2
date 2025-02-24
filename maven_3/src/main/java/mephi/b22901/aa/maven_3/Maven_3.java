fef4104a03e07b3454f2f3a20ef9a1000cd56fce
Darkindast <maxaondm77@yandex.ru>
2025-02-24 16:49:06 +0300

    commit0

---
+++ maven_3/src/main/java/mephi/b22901/aa/maven_3/Maven_3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mephi.b22901.aa.maven_3;
+
+/**
+ *
+ * @author Andrey
+ */
+public class Maven_3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
